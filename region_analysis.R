a1a56477e7a6e44364a2289b1442b665eb85ea30
kennedyzinn <125754918+kennedyzinn@users.noreply.github.com>
2025-04-02 13:58:27 -0500

    Update region_analysis.R

--- region_analysis.R
+++ region_analysis.R
@@ -1,19 +1,9 @@
-region_analysis <- function(contrast_coeff, gene_database, betacutoff, fdr){
-  myAnnotation <- cpg.annotate(object = Mval, datatype = "array", what = "M", 
+region_analysis <- function(contrast_coeff, betacutoff, fdr){
+  myAnnotation <- cpg.annotate(datatype = "array", object = Mval, what = "M", 
                                arraytype = c("EPICv1"), 
-                               analysis.type = "differential", design = design, contrasts = T, cont.matrix = contrast,
+                               analysis.type = "differential", design = fullSv, contrasts = T, cont.matrix = contrast,
                                fdr = fdr, coef = contrast_coeff)
   DMRs <- suppressMessages(dmrcate(myAnnotation, C=2, betacutoff = betacutoff))
-  results.ranges <- extractRanges(DMRs, genome = "hg19")
-  
-  # plot DMRs
-  if disease == disease
-  cols <- c(1,2)[disease]
-  names(cols) <-disease
-  par(mfrow=c(1,1))
-  DMR.plot(ranges=results.ranges, dmr=2, CpGs=beta_values, phen.col=cols, 
-           what="Beta", arraytype="EPICv1", genome="hg19")
-  gst.region <- goregion(results.ranges, all.cpg=rownames(Mval), 
-                         collection= gene_database, array.type="EPIC", plot.bias = T)
-  topGSA(gst.region)
+  results.ranges <- extractRanges(DMRs, genome = "hg38")
+  return(results.ranges)
 }
